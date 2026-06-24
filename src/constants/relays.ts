39057278bee5d115cdda43fd50f1c255ad529883
Vlad Stan <stan.v.vlad@gmail.com>
2026-06-24 17:39:53 +0300

    feat: add relay.nostr.com

--- src/constants/relays.ts
+++ src/constants/relays.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_RELAYS = [
+  'wss://relay.nostr.com',
   'wss://relay.damus.io',
   'wss://nostr.mom',
   'wss://nostr.bitcoiner.social',
   'wss://nos.lol',
-  'wss://relay.snort.social',
 ];
